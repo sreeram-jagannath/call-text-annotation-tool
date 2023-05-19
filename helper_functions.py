5cd752f7bae666a7ce3672de17c3ea0690e0baaa
Sreeram Jagannath <sreeram.subbaray@tigeranalytics.com>
2023-05-19 18:38:03 +0530

    updated reviewer page

--- helper_functions.py
+++ helper_functions.py
@@ -123,6 +123,57 @@ def save_data_to_table(
     # Commit the changes to the database
     conn.commit()
 
+def previous_button_clicked_reviewer():
+    idx = st.session_state["current_idx"] - 1
+
+    if idx == -1:
+        idx = st.session_state["n_chunks"] - 1
+    
+    st.session_state["current_idx"] = idx
+
+
+def next_button_clicked_reviewer():
+    idx = st.session_state["current_idx"] + 1
+
+    if idx == st.session_state["n_chunks"]:
+        idx = 0
+
+    st.session_state["current_idx"] = idx
+    
+
+def save_next_button_clicked_reviewer(
+    conn, cursor, new_id, selected_intents, selected_subintents, confidence, comment
+):
+    current_date = datetime.now().strftime("%Y-%m-%d")
+    current_time = datetime.now().strftime("%H:%M:%S")
+
+    sel_int_str = ", ".join(selected_intents)
+    sel_subint_str = ", ".join(selected_subintents)
+
+    user = st.session_state.get("name")
+    role = st.session_state.get("role")
+
+    save_data_to_table(
+        conn,
+        cursor,
+        new_id,
+        user,
+        role,
+        current_date,
+        current_time,
+        sel_int_str,
+        sel_subint_str,
+        confidence,
+        comment,
+    )
+
+    idx = st.session_state["current_idx"] + 1
+
+    if idx == st.session_state["n_chunks"]:
+        idx = 0
+    
+    st.session_state["current_idx"] = idx
+
 
 def previous_button_clicked():
     idx = st.session_state["current_idx"] - 1
@@ -198,3 +249,71 @@ def get_default_options(values):
 
     options = [s.strip() for s in values.split(",")]
     return options
+
+
+def get_call_ids_to_be_reviewed(call_data, user_call_mapping, annot_data, rev_username):
+    ...
+    """
+    read all possible calls
+    merge the mapping data to get the reviewer
+    merge it with annotated data
+    """
+
+    only_annotator_data = annot_data.query("role == 'annotator'")
+    only_reviewer_data = annot_data.query("role == 'reviewer'")
+
+    call_ids = (
+        pd.merge(call_data, user_call_mapping, on="ConnectionID", how="left")
+        .query("Reviewer == @rev_username")
+        .assign(
+            new_id=lambda x: x["ConnectionID"] + "_chunk_" + x["chunk_id"].astype(str)
+        )
+        .merge(
+            only_annotator_data, left_on=["new_id"], right_on=["call_id"], how="inner"
+        )
+        .sort_values(by=["ConnectionID", "chunk_id"])
+        .reset_index(drop=True)
+    )
+
+    # st.write("call data", call_data.shape, call_data)
+
+    # perform outer join
+    # outer = call_ids.merge(annotated_df, left_on="new_id", right_on='call_id', how='outer', indicator=True)
+    # st.write(outer)
+
+    # #perform anti-join
+    # anti_join = outer[(outer._merge=='both')].drop('_merge', axis=1)
+    # st.write(anti_join.head())
+
+    # st.write("call ids", call_ids.shape, call_ids)
+
+    return call_ids
+
+
+def get_already_reviewed_calls(_conn, connection_id, chunk_id):
+    name = st.session_state["name"]
+    new_id = f"{connection_id}_chunk_{chunk_id}"
+
+    query = f"SELECT * FROM call_annotation_table"
+    df = (
+        pd.read_sql_query(query, _conn)
+        .query("username == @name")
+        .query("call_id == @new_id")
+        .sort_values(["date", "time"], ascending=[False, False])
+        .reset_index(drop=True)
+    )
+
+    if df.empty:
+        status = "Pending"
+    else:
+        status = "Reviewed"
+
+    return status, df
+
+
+def reviewer_select_connid(review_df):
+    st.write(f"{st.session_state['conn_id_select']=}")
+
+    st.session_state["current_idx"] = review_df[
+        review_df["ConnectionID"] == st.session_state["conn_id_select"]
+    ].index[0]
--- review_page.py
+++ review_page.py
@@ -1,8 +1,194 @@
 import streamlit as st
 
+from helper_functions import *
+
 
 def get_reviewer_page():
-
+    st.markdown(
+        "<h1 style='text-align: center;'>Sunlife Annotation Tool</h1>",
+        unsafe_allow_html=True,
+    )
     st.write(f'Welcome *{st.session_state.get("name")}*')
-    st.write(f"Role: {st.session_state.get('role')}")
+    st.write(f"Role: *{st.session_state.get('role')}*")
 
+    data, intents, mapping = read_dataframes()
+    all_intents = get_all_intent_options(intent_df=intents)
+    all_subintents = get_all_subintent_options(intent_df=intents)
+
+    conn, cursor = init_connection()
+
+    already_annotated_df = read_annotated_data(_conn=conn)
+
+    # this can be some chunk of a call too...not necessarily the starting from a new call
+    review_call_ids = get_call_ids_to_be_reviewed(
+        call_data=data,
+        user_call_mapping=mapping,
+        annot_data=already_annotated_df,
+        rev_username=st.session_state.get("name"),
+    )
+
+    # st.write(annot_data)
+    # st.write(review_call_ids)
+
+    if review_call_ids.empty:
+        st.success("You don't have any texts to review!")
+        st.balloons()
+    else:
+        if "current_idx" not in st.session_state:
+            st.session_state["current_idx"] = 0
+            st.session_state["n_chunks"] = review_call_ids.shape[0]
+
+        current_row = review_call_ids.iloc[st.session_state["current_idx"]]
+        current_conn_id = current_row["ConnectionID"]
+        current_chunk_id = current_row["chunk_id"]
+
+        
+        if "conn_id_select" in st.session_state:
+            st.session_state["conn_id_select"] = current_conn_id
+
+        conn_id_list = review_call_ids["ConnectionID"].unique().tolist()
+        st.selectbox(
+            "Connection ID",
+            options=conn_id_list,
+            key="conn_id_select",
+            on_change=reviewer_select_connid,
+            args=(review_call_ids,),
+        )
+        
+
+        with st.expander(
+            label=f"Expand to see full conversation (ConnectionID: {current_conn_id})"
+        ):
+            full_text = current_row["full_text"]
+            st.text(full_text)
+
+        st.markdown(
+            "<h3 style='text-align: center;'>Review annotations</h3>",
+            unsafe_allow_html=True,
+        )
+
+        # progress_text = f"Progress: [{len(st.session_state['annotated_idx'])} / {st.session_state['n_chunks']}]"
+        # st.progress(value=len(st.session_state["annotated_idx"])/st.session_state["n_chunks"], text=progress_text)
+
+        st.markdown(
+            "<h3 style='text-align: center;'>Annotation details</h3>",
+            unsafe_allow_html=True,
+        )
+
+        # Text display
+        st.markdown(
+            f"<p style='text-align: center;'>{current_row['text']}</p>",
+            unsafe_allow_html=True,
+        )
+        # st.write(f"ConnectionID: {current_conn_id} ChunkID: {current_row['chunk_id']}", )
+
+        st.info("**Annotation Details**")
+        req_cols = [
+            "ConnectionID",
+            "chunk_id",
+            "Annotator",
+            "case_type",
+            "subcase_type",
+            "confidence",
+            "comments",
+        ]
+        st.table(current_row[req_cols].T)
+        # st.write(f"**Annotated by**: {current_row['Annotator']}")
+        # st.write(f"**Intents**: {current_row['case_type']}")
+        # st.write(f"**Sub Intents**: {current_row['subcase_type']}")
+        # st.write(f"**Confidence**: {current_row['confidence']}")
+        # st.write(f"**Annotator Comments**: {current_row['comments']}")
+
+        # st.write(current_row["Call Type"].split(','), all_intents)
+        # st.write(all_intents, [] if current_row["Call Type"] is None else current_row["Call Type"].split(','))
+
+        status, reviewed_df = get_already_reviewed_calls(
+            _conn=conn, connection_id=current_conn_id, chunk_id=current_chunk_id
+        )
+        st.info(f"Reviewer Status: {status}")
+        st.table(reviewed_df)
+
+        # Dropdowns
+        _, scol1, scol2, _ = st.columns([1, 1, 1, 1])
+        default_intents = get_default_options(current_row["case_type"])
+        intent_list = scol1.multiselect(
+            label="Intent",
+            options=all_intents,
+            default=default_intents,
+            key=f"intent_dropdown_{current_row['new_id']}",
+        )
+
+        valid_subintents = get_valid_subintent_options(
+            intent_list=intent_list, subintent_map=all_subintents
+        )
+        default_subintents = get_default_options(current_row["subcase_type"])
+        final_default_subintents = list(
+            set(valid_subintents).intersection(set(default_subintents))
+        )
+
+        # st.write(f"Valid Subintents: {valid_subintents} || Default Subintents: {default_subintents} Final Default: {final_default_subintents}")
+        subintent_list = scol2.multiselect(
+            label="Sub Intent",
+            options=valid_subintents,
+            default=final_default_subintents,
+            key=f"subintent_dropdown_{current_row['new_id']}",
+        )
+
+        _, conf_col, _ = st.columns([1, 2, 1])
+        confidence_level = conf_col.selectbox(
+            label="Confidence",
+            options=["High", "Medium", "Low"],
+            key=f"conf_sel_{current_row['new_id']}",
+        )
+
+        _, textcol, _ = st.columns([1, 2, 1])
+        reviewer_comments = textcol.text_area(
+            label="Reviewer Comments", key=f"comment_{current_row['new_id']}", height=10
+        )
+
+        # st.write(st.session_state)
+
+        st.markdown(
+            "<h4 style='text-align: center;'>Final selection</h4>",
+            unsafe_allow_html=True,
+        )
+
+        # display choices in columns
+        _, ccol1, ccol2, _ = st.columns([1, 2, 2, 1])
+        ccol1.info(f"**Intents**: {intent_list}")
+        ccol2.warning(f"**Sub Intents**: {subintent_list}")
+
+        st.title("")
+        _, bcol1, bcol2, bcol3, _ = st.columns([1.5, 1, 1, 1, 1])
+
+        if st.session_state["current_idx"] > 0:
+            bcol1.button("Previous", on_click=previous_button_clicked_reviewer)
+
+        # if done with all the chunks for the user, don't show the save and next button
+        # if st.session_state["current_idx"] + 1 < len(call_ids):
+        bcol2.button("Next", on_click=next_button_clicked_reviewer)
+
+        bcol3.button(
+            "Save and Next",
+            on_click=save_next_button_clicked_reviewer,
+            args=(
+                conn,
+                cursor,
+                current_row["new_id"],
+                intent_list,
+                subintent_list,
+                confidence_level,
+                reviewer_comments,
+            ),
+        )
+
+        if st.button("close_database"):
+            cursor.close()
+            conn.close()
+            st.stop()
+
+        if st.button("Read database"):
+            query = f"SELECT * FROM call_annotation_table"
+            df = pd.read_sql_query(query, conn)
+
+            st.write(df)
